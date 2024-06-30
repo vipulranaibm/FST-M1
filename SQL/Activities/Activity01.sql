8377f0a9ef406d3a8148b012d4892f0f37e6e5f0
vipulranaibm <vipurana@in.ibm.com>
2024-06-30 21:16:18 +0530

    Add files via upload

---
+++ SQL/Activities/Activity01.sql
@@ -0,0 +1,10 @@
+REM   Script: Activity1
+REM   Activity 01
+
+Create table salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(20), 
+    salesman_city varchar2(20), 
+    commission int 
+);
+
